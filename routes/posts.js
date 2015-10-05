0dbac643fde2f650b7de05d0715022cbed95d4ec
enriquesleon <enriquesleon>
2015-10-04 21:40:01 -0700

    added post routing

--- routes/posts.js
+++ routes/posts.js
@@ -11,4 +11,14 @@ router.get('/', function(req, res, next) {
     res.json(posts);
   });
 });
+router.post('/', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+});
+
 module.exports = router;
