63dea6dc16d6bbc638744e9bfe6d90d7286de3a9
Viet Truong <viettruong2194@gmail.com>
2015-11-11 22:54:39 -0800

    Fix homeController rendering issues

--- public/javascripts/controllers/homeController.js
+++ public/javascripts/controllers/homeController.js
@@ -1,28 +1,12 @@
 angular.module('flapperNews.controllers').
 controller('MainCtrl', [
     '$scope',
-    'posts',
     'incidents',
     'auth',
-    function($scope, posts, incidents, auth) {
+    function($scope, incidents, auth) {
         $scope.test = 'Hello World!';
-        $scope.posts = posts.posts;
         $scope.incidents = incidents.incidents;
         $scope.isLoggedIn = auth.isLoggedIn;
-
-        $scope.addPost = function(){
-          if(!$scope.title || $scope.title === '') { return; }
-          posts.create({
-            title: $scope.title,
-            link: $scope.link,
-          });
-          $scope.title = '';
-          $scope.link = '';
-        };
-
-        $scope.incrementUpvotes = function(post) {
-            posts.upvote(post);
-        };
 
         $scope.addIncident = function() {
             if (!$scope.building_type || $scope.building_type === '' || !$scope.address | $scope.address === '') return;
